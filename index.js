2b421fcdcad7e7849a4825b7215f36ab5a7c9dbd
totmak <totmak@utu.fi>
2023-01-23 18:48:25 +0200

    yml test

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-//const io = new Server(server);
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["CONNECT", "GET", "POST"]
-  }
-});
-
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-
-
-server.listen(5000, () => {
-  console.log('listening on *:3000');
-});
-
-
-io.on('connection', (socket) => {
-  number++;
-  console.log(number);
-
-  socket.on('submit', (msg) => {
-    console.log("sasasa")
-    console.log(msg);
-  });
-});
-
-let number = -1;
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
